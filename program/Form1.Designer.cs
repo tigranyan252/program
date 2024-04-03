7f04ced64b0e5a628d5446800a85cd9385c13162
Serj Tigranyan <serj-tigranyan@mail.ru>
2024-04-04 01:37:46 +0200

    Добавьте файлы проекта.

---
+++ program/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBoxProcesses = new System.Windows.Forms.ListBox();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonKillProcess = new System.Windows.Forms.Button();
+            this.timerUpdate = new System.Windows.Forms.Timer(this.components);
+            this.lblDetails = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxProcesses
+            // 
+            this.listBoxProcesses.FormattingEnabled = true;
+            this.listBoxProcesses.Location = new System.Drawing.Point(12, 12);
+            this.listBoxProcesses.Name = "listBoxProcesses";
+            this.listBoxProcesses.Size = new System.Drawing.Size(301, 238);
+            this.listBoxProcesses.TabIndex = 0;
+            this.listBoxProcesses.SelectedIndexChanged += new System.EventHandler(this.listBoxProcesses_SelectedIndexChanged);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(346, 227);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(109, 23);
+            this.buttonRefresh.TabIndex = 1;
+            this.buttonRefresh.Text = "ButtonRefresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonKillProcess
+            // 
+            this.buttonKillProcess.Location = new System.Drawing.Point(634, 227);
+            this.buttonKillProcess.Name = "buttonKillProcess";
+            this.buttonKillProcess.Size = new System.Drawing.Size(140, 23);
+            this.buttonKillProcess.TabIndex = 2;
+            this.buttonKillProcess.Text = "buttonKillProcess";
+            this.buttonKillProcess.UseVisualStyleBackColor = true;
+            this.buttonKillProcess.Click += new System.EventHandler(this.buttonKillProcess_Click);
+            // 
+            // timerUpdate
+            // 
+            this.timerUpdate.Tick += new System.EventHandler(this.timerUpdate_Tick);
+            // 
+            // lblDetails
+            // 
+            this.lblDetails.AutoSize = true;
+            this.lblDetails.Location = new System.Drawing.Point(725, 12);
+            this.lblDetails.Name = "lblDetails";
+            this.lblDetails.Size = new System.Drawing.Size(49, 13);
+            this.lblDetails.TabIndex = 3;
+            this.lblDetails.Text = "lblDetails";
+            this.lblDetails.Click += new System.EventHandler(this.lblDetails_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDetails);
+            this.Controls.Add(this.buttonKillProcess);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.listBoxProcesses);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxProcesses;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonKillProcess;
+        private System.Windows.Forms.Timer timerUpdate;
+        private System.Windows.Forms.Label lblDetails;
+    }
+}
+
---
+++ program/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            UpdateProcessList();
+        }
+
+        private void UpdateProcessList()
+        {
+            listBoxProcesses.DataSource = Process.GetProcesses()
+        .Select(p => p.ProcessName)
+        .Distinct()
+        .OrderBy(p => p)
+        .ToList();
+        }
+
+        private void timerUpdate_Tick(object sender, EventArgs e)
+        {
+            UpdateProcessList();
+        }
+
+        private void listBoxProcesses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxProcesses.SelectedIndex >= 0)
+            {
+                var selectedProcessName = listBoxProcesses.SelectedItem as string;
+                var selectedProcesses = Process.GetProcessesByName(selectedProcessName);
+
+                if (selectedProcesses.Any())
+                {
+                    lblDetails.Text = GetProcessDetails(selectedProcesses[0]);
+                }
+            }
+        }
+
+        private string GetProcessDetails(Process process)
+        {
+            return $"ID: {process.Id}\n" +
+                   $"Start time: {process.StartTime}\n" +
+                   $"Total processor time: {process.TotalProcessorTime}\n" +
+                   $"Threads count: {process.Threads.Count}\n" +
+                   $"Instances count: {Process.GetProcessesByName(process.ProcessName).Length}";
+        }
+
+        private void lblDetails_Click(object sender, EventArgs e)
+        {
+            UpdateProcessList();
+            timerUpdate.Interval = 1000; // Update interval in milliseconds
+            timerUpdate.Start();
+
+        }
+
+        private void buttonKillProcess_Click(object sender, EventArgs e)
+        {
+            if (listBoxProcesses.SelectedIndex >= 0)
+            {
+                var selectedProcessName = listBoxProcesses.SelectedItem as string;
+                var selectedProcesses = Process.GetProcessesByName(selectedProcessName);
+
+                if (selectedProcesses.Any())
+                {
+                    selectedProcesses[0].Kill();
+                    UpdateProcessList();
+                }
+            }
+        }
+    }
+}
